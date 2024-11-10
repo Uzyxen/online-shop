9850990d5ac0fdf3a47e0d224b78feb7196f7034
Uzyxen <michal912456@gmail.com>
2024-11-10 18:20:27 +0100

    chore: add Tailwindcss module and some custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors: {
+      'light-gray': '#EEEEEE',
+      'blue': '#1976D2',
+      'black': '#1E1E1E'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
