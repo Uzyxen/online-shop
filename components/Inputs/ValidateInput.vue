93dfa09fe483ed1d2c077aeda5033227696d8b5f
Uzyxen <michal912456@gmail.com>
2024-11-14 00:49:14 +0100

    styles

--- components/Inputs/ValidateInput.vue
+++ components/Inputs/ValidateInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col gap-3">
         <p class="text-base font-semibold">{{ caption }}</p>
-        <input :type="type" class="w-[600px] p-3 bg-gray">
+        <input :type="type" class="w-[600px] p-3 bg-gray outline-none">
     </div>
 </template>
 
