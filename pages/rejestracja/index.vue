d3bfbdf07a1f7ca6841fa9f3cb3d334d55314d0f
Uzyxen <michal912456@gmail.com>
2024-11-14 00:34:06 +0100

    create register page

--- pages/rejestracja/index.vue
+++ pages/rejestracja/index.vue
@@ -1,20 +1,21 @@
 <template>
     <div class="flex h-[100vh]">
         <div class="w-3/5 flex flex-col justify-center items-center">
-            <h2 class="text-5xl font-semibold text-blue mb-24">Zaloguj się</h2>
+            <h2 class="text-5xl font-semibold text-blue mb-24">Zarejestruj się</h2>
 
             <form class="flex flex-col gap-12">
                 <ValidateInput caption="E-mail:" type="email" />
+                <ValidateInput caption="Hasło:" type="password" />
 
                 <div>
-                    <ValidateInput caption="Hasło:" type="password" />
+                    <ValidateInput caption="Potwórz hasło:" type="password" />
                     <p class="mt-6">
-                        Nie masz konta? 
-                        <NuxtLink to="/rejestracja">Zrejestruj się</NuxtLink>
+                        Masz już konto? 
+                        <NuxtLink to="/logowanie">Zaloguj się</NuxtLink>
                     </p>
                 </div>
 
-                <PrimaryButton class="w-full mt-16 h-12">Zaloguj się</PrimaryButton>
+                <PrimaryButton class="w-full mt-16 h-12">Utwórz konto</PrimaryButton>
             </form>
         </div>
 
