c4aee3d2a80caeff8bd8fb5f349062b3aa39e1be
Uzyxen <michal912456@gmail.com>
2024-11-14 17:35:15 +0100

    fix: recover CenterHeader.vue component

--- components/Inputs/SearchBar.vue
+++ components/Inputs/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-10 flex">
-        <input type="search" placeholder="Wyszukaj..." class="outline-none h-full text-sm border-gray border-2 border-r-0 bg-light-gray w-[450px] px-2 focus:border-blue">
+        <input type="search" placeholder="Wyszukaj..." class="outline-none h-full text-sm border-dark-gray border-2 border-r-0 bg-light-gray w-[450px] px-2 focus:border-blue">
         <button class="bg-blue text-light-gray h-full px-2 flex items-center justify-center">
             <NuxtIcon name="mol-icon:forward" size="1.7rem" />
         </button>
---
+++ components/headers/CenterHeader.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-4xl font-semibold text-blue mb-24">
+        <slot />
+    </h1>
+</template>
