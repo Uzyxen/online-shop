22acf92006f3a94bb939fe5404f40634c77ff03b
Uzyxen <michal912456@gmail.com>
2024-11-14 17:26:14 +0100

    create header as a component

--- components/Inputs/ValidateInput.vue
+++ components/Inputs/ValidateInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col gap-3">
         <p class="text-base font-semibold">{{ caption }}</p>
-        <input :type="type" class="w-[600px] p-3 bg-gray outline-none" v-model="model">
+        <input :type="type" class="w-[500px] p-2.5 bg-gray outline-none" v-model="model">
     </div>
 </template>
 
--- pages/rejestracja/index.vue
+++ pages/rejestracja/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex h-[100vh]">
         <div class="w-3/5 flex flex-col justify-center items-center">
-            <h2 class="text-5xl font-semibold text-blue mb-24">Zarejestruj się</h2>
+            <CenterHeader>Zarejestruj się</CenterHeader>
 
             <form class="flex flex-col gap-12">
                 <ValidateInput caption="E-mail:" type="email" v-model="registerData.email" />
