fdc5035ac51e91c8de5bd5bf0e91dedb7b64a4f9
Uzyxen <michal912456@gmail.com>
2024-11-16 13:32:42 +0100

    create products simple schema

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,7 +1,13 @@
-import { int, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
+import { decimal, int, mysqlTable, varchar } from 'drizzle-orm/mysql-core';
 
 export const users = mysqlTable('users', {
     id: int().notNull().primaryKey().autoincrement(),
     email: varchar({ length: 255 }).notNull().unique(),
     password: varchar({ length: 255 }).notNull(),
 });
+
+export const products = mysqlTable('products', {
+    id: int().notNull().primaryKey().autoincrement(),
+    name: varchar({ length: 100 }).notNull(),
+    price: decimal({ precision: 10, scale: 2 }) // decimal(10, 2) -> 99999999.99
+})
