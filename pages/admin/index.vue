eb75923777571b656d982d50ebda318382385cf8
Uzyxen <michal912456@gmail.com>
2025-01-05 23:26:25 +0100

    add hover transitions

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="mx-20">
         <ul class="grid gap-2 mt-4 grid-cols-5">
-            <li v-for="(page, index) in adminPages" :key="index" class="border-2 border-gray h-52">
-                <NuxtLink :to="page.path" class="flex flex-col gap-4 p-5 justify-center items-center size-full bg-light-gray group hover:bg-blue">
-                    <NuxtIcon :name="page.icon" size="7rem"/>
+            <li 
+                v-for="(page, index) in adminPages" 
+                :key="index" 
+                class="border-2 border-gray h-52 relative after:bg-blue after:w-full after:content-[''] after:absolute after:bottom-0 after:h-0 hover:after:h-full group cursor-pointer after:transition-all"
+                @mouseover="page.hover = true"
+                @mouseleave="page.hover = false">
+                <NuxtLink 
+                    :to="page.path" 
+                    class="flex flex-col gap-4 p-5 justify-center items-center size-full bg-light-gray z-50">
+                    <NuxtIcon :name="page.icon" size="7rem" :style="{ color: page.hover ? '#FFF' : '#000' }" class="z-10 transition-colors duration-300"/>
 
-                    <h1 class="text-lg group-hover:text-[#FFF]">{{ page.title }}</h1>
+                    <h1 class="text-lg group-hover:text-[#FFF] z-10 transition-colors duration-300">{{ page.title }}</h1>
                 </NuxtLink>
             </li>
         </ul>
@@ -17,10 +24,12 @@
         middleware: 'auth-admin'
     });
 
+    const isHovered = ref(false);
+
     const adminPages = ref([
-        { title: 'Zarządzanie produktami', path: '/admin/produkty', icon: 'carbon:ibm-data-product-exchange' },
-        { title: 'Zarządzanie kategoriami', path: '/admin/kategorie', icon: 'carbon:collapse-categories' },
-        { title: 'Zarządzanie zamówieniami', path: '/admin/zamowienia', icon: 'mdi:basket-unfill' },
+        { title: 'Zarządzanie produktami', path: '/admin/produkty', icon: 'carbon:ibm-data-product-exchange', hover: false },
+        { title: 'Zarządzanie kategoriami', path: '/admin/kategorie', icon: 'carbon:collapse-categories', hover: false },
+        { title: 'Zarządzanie zamówieniami', path: '/admin/zamowienia', icon: 'mdi:basket-unfill', hover: false },
     ]);
     
 </script>
