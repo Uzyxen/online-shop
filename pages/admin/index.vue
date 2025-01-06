9aaa67a656b8550dd362240133f2c0160ff608e9
Uzyxen <michal912456@gmail.com>
2025-01-06 22:49:55 +0100

    fix: fix <li> that was not redirecting to the correct page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,15 +5,13 @@
                 v-for="(page, index) in adminPages" 
                 :key="index" 
                 class="border-2 border-gray h-52 rounded-sm relative after:bg-blue after:w-full after:content-[''] after:absolute after:bottom-0 after:h-0 hover:after:h-full hover:border-blue transition-colors group cursor-pointer after:transition-all"
-                @mouseover="page.hover = true"
-                @mouseleave="page.hover = false">
-                <NuxtLink 
-                    :to="page.path" 
+                @click="navigateTo(page.path)">
+                <div
                     class="flex flex-col gap-4 p-5 justify-center items-center size-full bg-light-gray z-20">
                     <NuxtIcon :name="page.icon" size="6rem" mode="svg" class="z-10 transition-colors duration-300 group-hover:text-[#FFF]"/>
 
                     <h1 class="text-lg group-hover:text-[#FFF] z-10 transition-colors duration-300 text-black">{{ page.title }}</h1>
-                </NuxtLink>
+                </div>
             </li>
         </ul>
     </div>
