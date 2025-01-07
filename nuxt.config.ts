293cdc9a265e30b8de8a686d06862db40bbc3f4e
Uzyxen <michal912456@gmail.com>
2025-01-07 16:30:18 +0100

    temp fix: set esbuild option to esnext

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,12 @@ export default defineNuxtConfig({
     jwtAccessSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_TOKEN_SECRET
   },
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext'
+      }
+    }
+  },
   ssr: true
 })
