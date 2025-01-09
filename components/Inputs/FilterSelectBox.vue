b4c1601b31acd94d3978c2f864438ced74df0f5e
Uzyxen <michal912456@gmail.com>
2025-01-09 21:28:05 +0100

    fix: replace v-if with array swapping to delete items loading delay

--- components/Inputs/FilterSelectBox.vue
+++ components/Inputs/FilterSelectBox.vue
@@ -2,12 +2,11 @@
     <div class="bg-mid-gray w-full p-2.5 relative" @click="itemsVisible = !itemsVisible">
         <div class="flex w-full justify-between items-center">
             <span>{{ selectedIndex !== null ? items[selectedIndex].title : 'Wybierz' }} {{ selectedSubItem !== null ? ' / ' + subItems[selectedSubItem].title : '' }}</span>
-            <NuxtIcon v-if="selectedIndex !== null" name="solar:arrow-left-linear" size="1.5rem" mode="svg" @click.stop="selectedIndex = null; selectedSubItem = null" />
+            <NuxtIcon v-if="selectedIndex !== null" name="solar:arrow-left-linear" size="1.5rem" mode="svg" @click.stop="selectedIndex = null; selectedSubItem = null; itemsToDisplay = items" />
         </div>
 
         <ul v-if="itemsVisible" class="absolute bg-mid-gray w-full left-0 top-full shadow-sm">
-            <li v-if="selectedIndex === null" v-for="(item, index) in items" :key="item.id" class="p-2 hover:bg-gray cursor-pointer" @click.stop="selectItem(index)">{{ item.title }}</li>
-            <li v-else v-for="(subItem, index) in subItems" :key="subItem.id" class="p-2 hover:bg-gray cursor-pointer" @click.stop="selectSubItem(index)">{{ subItem.title }}</li>
+            <li v-for="(item, index) in itemsToDisplay" :key="item.id" class="p-2 hover:bg-gray cursor-pointer" @click.stop="selectItem(index)">{{ item.title }}</li>
         </ul>
     </div>
 </template>
@@ -15,25 +14,34 @@
 <script setup>
     const props = defineProps(['endpoint', 'subEndpoint']);
 
+    const itemsToDisplay = ref([]);
+
     const { data: items } = await useFetch(props.endpoint, {
         method: 'post'
     });
 
-    const subItems = ref([]);
+    itemsToDisplay.value = items.value;
 
+    const subItems = ref([]);
     const itemsVisible = ref(false);
     const selectedIndex = ref(null);
     const selectedSubItem = ref(null);
 
-    async function selectItem(index) {        
-        selectedIndex.value = index;
+    async function selectItem(index) {
+        if(selectedIndex.value === null) {
+            selectedIndex.value = index;
         
-        subItems.value = await $fetch(props.subEndpoint, {
-            method: 'post',
-            body: {
-                name: items.value[index].title
-            }
-        });
+            subItems.value = await $fetch(props.subEndpoint, {
+                method: 'post',
+                body: {
+                    name: items.value[index].title
+                }
+            });
+
+            itemsToDisplay.value = subItems.value;
+        } else {
+            selectSubItem(index);
+        }
     }
 
     async function selectSubItem(index) {        
