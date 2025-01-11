367d44e51fbf97e67ce7298d749b87bf18af2a69
Uzyxen <michal912456@gmail.com>
2025-01-11 21:58:06 +0100

    create page index input

--- components/navigation/PaginationButtons.vue
+++ components/navigation/PaginationButtons.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="mb-20">
+    <div class="mb-20 flex gap-3">
         <button @click="index--; $emit('pageChange', (index - 1) * 20)">
             Poprzednia strona
         </button>
 
-        {{ index }}
+        <input type="text" v-model="index" class="border border-dark-gray w-10 text-center p-1" />
 
         <button @click="index++; $emit('pageChange', (index - 1) * 20)">
             Następna strona
