c964c14791249c711f9e089125dab153470fd533
Uzyxen <michal912456@gmail.com>
2025-01-24 16:10:39 +0100

    increase number of columns for featured products

--- components/UI/FeaturedProducts.vue
+++ components/UI/FeaturedProducts.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-7xl mx-auto flex flex-col items-center gap-14">
-        <div class="mt-8 w-full grid grid-cols-3 gap-7 justify-center">
-            <div class="bg-gray h-[250px]" v-for="i in 6"></div>
+        <div class="mt-8 w-full grid grid-cols-4 gap-7 justify-center">
+            <div class="bg-gray h-[200px]" v-for="i in 8"></div>
         </div>
 
         <PrimaryButton>Zobacz więcej</PrimaryButton>
