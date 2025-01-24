36628645ba9038cfd309a0611b705dcc15e36f9e
Uzyxen <michal912456@gmail.com>
2025-01-24 22:18:50 +0100

    user page: add list of available pages

--- pages/konto/index.vue
+++ pages/konto/index.vue
@@ -1,6 +1,15 @@
 <template>
-    <div class="px-20">
-        
+    <div class="mx-20 pb-16">
+        <HorizontalTextLine>Konto</HorizontalTextLine>
+        <ul class="grid gap-2 mt-4 grid-cols-6">
+            <AnimatedBlock :page="{ title: 'Moje zamówienia', icon: 'solar:clipboard-list-linear', path: '/konto/zamowienia' }" />
+            <AnimatedBlock :page="{ title: 'Zakupione produkty', icon: 'solar:box-minimalistic-linear', path: '/konto/produkty' }" />
+            <AnimatedBlock :page="{ title: 'Zapisane produkty', icon: 'solar:heart-linear', path: '/konto/zapisane' }" />
+            <AnimatedBlock :page="{ title: 'Ksiąkża adresowa', icon: 'solar:book-2-linear', path: '/konto/ksiazka-adresowa' }" />
+            <AnimatedBlock :page="{ title: 'Zapisane konfiguracje', icon: 'solar:monitor-linear', path: '/konto/konfiguracje-pc' }" />
+            <AnimatedBlock :page="{ title: 'Ustawienia', icon: 'solar:settings-linear', path: '/konto/ustawienia' }" />
+            <AnimatedBlock :page="{ title: 'Bezpieczeństwo', icon: 'solar:lock-linear', path: '/konto/bezpieczenstwo' }" />
+        </ul>
     </div>
 </template>
 
