c7b748b42022871f1bf17b317c1759a14dd899a3
Uzyxen <michal912456@gmail.com>
2025-01-25 22:49:18 +0100

    create user address block

---
+++ components/blocks/UserAddress.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="rounded-sm w-full h-44 border-2 border-blue-gray">
+
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        address: Object
+    })
+</script>
--- pages/konto/ksiazka-adresowa/index.vue
+++ pages/konto/ksiazka-adresowa/index.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        
-    </div>
+    <section class="grid grid-cols-5 gap-4 mx-20">
+        <UserAddress v-for="address in addresses" :key="address.id" :address="address" />
+    </section>
 </template>
+
+<script setup>
+    const addresses = [
+        {
+            id: 1,
+            name: 'John Doe',
+            street: '1234 Main St',
+            city: 'Springfield',
+            state: 'IL',
+            zip: '62701',
+            country: 'USA'
+        },
+        {
+            id: 2,
+            name: 'Jane Doe',
+            street: '5678 Elm St',
+            city: 'Springfield',
+            state: 'IL',
+            zip: '62701',
+            country: 'USA'
+        }
+    ]
+</script>
