e629a3c65c7882ffd61acda0f23a6510a15dc464
Uzyxen <michal912456@gmail.com>
2025-01-25 23:14:02 +0100

    add checkbox

--- components/blocks/UserAddress.vue
+++ components/blocks/UserAddress.vue
@@ -1,11 +1,15 @@
 <template>
     <div 
-        class="rounded-sm w-full h-44 border-2 border-blue-gray cursor-pointer hover:border-blue hover:bg-blue-gray-light transition-colors" 
+        class="rounded-sm w-full h-48 border-2 border-blue-gray cursor-pointer hover:border-blue hover:bg-blue-gray-light transition-colors" 
         :class="{ 'bg-blue-gray-light': address.selected }">
-        <div class="size-full p-4 flex flex-col gap-5">
-            <h1 class="font-medium text-lg">{{ address.firstName }} {{ address.lastName }}</h1>
+        <div class="size-full p-4 flex flex-col relative">
+            <div>
+                <input type="checkbox" :checked="address.selected" class="absolute top-2 right-2" />
+            </div>
 
-            <div class="flex flex-col gap-1">
+            <h1 class="font-medium text-lg mt-3">{{ address.firstName }} {{ address.lastName }}</h1>
+
+            <div class="flex flex-col gap-1 mt-5">
                 <h4>{{ address.street }} {{ address.streetNumber }}</h4>
                 <h4>{{ address.zip }} {{ address.city }}</h4>
                 <h4>{{ address.phoneNumber }}</h4>
