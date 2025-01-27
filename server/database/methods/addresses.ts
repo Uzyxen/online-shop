c6dc8e969449ab01ebb502657526f8ca26d63725
Uzyxen <michal912456@gmail.com>
2025-01-27 01:08:38 +0100

    cache user addresses

--- server/database/methods/addresses.ts
+++ server/database/methods/addresses.ts
@@ -2,10 +2,12 @@ import { eq } from "drizzle-orm";
 import db from "../connection";
 import { addressesTable } from "../schema";
 
-export const getAllUserAddresses = async (id: number) => {
+export const getAllUserAddresses = defineCachedFunction(async (id: number) => {
     const result = await db.query.addressesTable.findMany({
         where: eq(addressesTable.userId, id)
     });
 
     return result;
-}
+}, {
+    maxAge: 5 * 60
+});
