c022b5603bf0f18760599221430a6facd6155529
Uzyxen <michal912456@gmail.com>
2025-01-28 23:06:44 +0100

    style sessions

--- components/blocks/SessionBlock.vue
+++ components/blocks/SessionBlock.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        {{ session.createdAt }}
+    <div class="border border-t-0 border-blue-gray p-2 flex first:border-t">
+        <h1 class="flex-1">{{ session.createdAt }}</h1>
+
+        <button>Wyloguj</button>
     </div>
 </template>
 
