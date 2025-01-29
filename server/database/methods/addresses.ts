b72f6015da01d50d9011c928eea4c5b902d12cc8
Uzyxen <michal912456@gmail.com>
2025-01-29 22:38:04 +0100

    Temporarily disable caching user addresses

--- server/database/methods/addresses.ts
+++ server/database/methods/addresses.ts
@@ -2,12 +2,10 @@ import { eq } from "drizzle-orm";
 import db from "../connection";
 import { addressesTable } from "../schema";
 
-export const getAllUserAddresses = defineCachedFunction(async (id: number) => {
+export const getAllUserAddresses = async (id: number) => {
     const result = await db.query.addressesTable.findMany({
         where: eq(addressesTable.userId, id)
     });
 
     return result;
-}, {
-    maxAge: 5 * 60
-});
+}
