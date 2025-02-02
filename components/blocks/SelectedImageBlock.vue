c26b979eb5c57fe15c7703b5d8fe17940b71c486
Uzyxen <michal912456@gmail.com>
2025-02-02 19:35:49 +0100

    add ::after

--- components/blocks/SelectedImageBlock.vue
+++ components/blocks/SelectedImageBlock.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="w-full flex justify-center items-center border border-dark-gray max-h-40">
+    <div 
+        class="w-full flex justify-center items-center border relative
+        border-dark-gray max-h-40 after:content-[''] after:bg-black-40 after:absolute after:size-full">
         <img :src="image.url" :alt="image.alt" class="size-full object-cover">
     </div>
 </template>
