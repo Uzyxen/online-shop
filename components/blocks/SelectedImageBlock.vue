b27870e922fe4d8f6961e6617a6aade970393884
Uzyxen <michal912456@gmail.com>
2025-02-02 19:36:48 +0100

    add color

--- components/blocks/SelectedImageBlock.vue
+++ components/blocks/SelectedImageBlock.vue
@@ -1,7 +1,7 @@
 <template>
     <div 
         class="w-full flex justify-center items-center border relative
-        border-dark-gray max-h-40 after:content-[''] after:bg-black-40 after:absolute after:size-full">
+        border-dark-gray max-h-40 after:content-[''] after:bg-black-50 after:absolute after:size-full">
         <img :src="image.url" :alt="image.alt" class="size-full object-cover">
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,7 @@ export default {
       'blue-dark': '#0D47A1',
       'blue-30': '#E3F2FD',
       'black': '#061F33',
+      'black-50': 'rgba(6, 31, 51, 0.5)',
       'black-40': 'rgba(6, 31, 51, 0.3)',
       'black-10': 'rgba(6, 31, 51, 0.1)',
       'red': '#FF0F0F'
