003b61b75f585a488f371136646ef9d875d36029
Uzyxen <michal912456@gmail.com>
2025-02-04 23:32:36 +0100

    responsive grid

--- pages/konto/index.vue
+++ pages/konto/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="px-20 pb-16">
+    <div class="px-3 sm:px-5 md:px-10 px-20 pb-16">
         <HorizontalTextLine>Konto</HorizontalTextLine>
-        <ul class="grid gap-2 mt-4 grid-cols-6">
+        <ul class="grid gap-2 mt-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             <AnimatedBlock :page="{ title: 'Moje zamówienia', icon: 'solar:clipboard-list-linear', path: '/konto/zamowienia' }" />
             <AnimatedBlock :page="{ title: 'Zakupione produkty', icon: 'solar:box-minimalistic-linear', path: '/konto/produkty' }" />
             <AnimatedBlock :page="{ title: 'Zapisane produkty', icon: 'solar:heart-linear', path: '/konto/zapisane' }" />
