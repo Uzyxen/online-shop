500ddc40717a1ee8062c40da8c21b18640466cdf
Uzyxen <michal912456@gmail.com>
2025-02-06 20:50:38 +0100

    responsive grid

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="mx-20 pb-16">
         <HorizontalTextLine>Produkty</HorizontalTextLine>
-        <ul class="grid gap-2 mt-4 grid-cols-6">
+        <ul class="grid gap-2 mt-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             <AnimatedBlock :page="{ title: 'Dodaj nowy produkt', icon: 'solar:widget-add-linear', path: '/admin/dodaj-produkt' }" />
             <AnimatedBlock :page="{ title: 'Przeglądaj produkty', icon: 'solar:box-minimalistic-linear', path: '/admin/produkty' }" />
         </ul>
 
         <HorizontalTextLine class="mt-10">Kategorie</HorizontalTextLine>
-        <ul class="grid gap-2 mt-4 grid-cols-6">
+        <ul class="grid gap-2 mt-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             <AnimatedBlock :page="{ title: 'Zarządzanie kategoriami', icon: 'solar:folder-with-files-linear', path: '/admin/kategorie' }" />
         </ul>
 
         <HorizontalTextLine class="mt-10">Zamówienia</HorizontalTextLine>
-        <ul class="grid gap-2 mt-4 grid-cols-6">
+        <ul class="grid gap-2 mt-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             <AnimatedBlock :page="{ title: 'Przeglądaj zamówienia', icon: 'solar:cart-linear', path: '/admin/zamowienia' }" />
             <AnimatedBlock :page="{ title: 'Przeglądaj zwroty', icon: 'solar:undo-left-round-linear', path: '/admin/zwroty' }" />
         </ul>
 
         <HorizontalTextLine class="mt-10">Zgłoszenia uytkowników</HorizontalTextLine>
-        <ul class="grid gap-2 mt-4 grid-cols-6">
+        <ul class="grid gap-2 mt-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             <AnimatedBlock :page="{ title: 'Przeglądaj zgłoszenia', icon: 'solar:danger-circle-linear', path: '/admin/zgloszenia' }" />
         </ul>
     </div>
