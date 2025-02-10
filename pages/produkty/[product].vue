570e808dc7d719351cd084466dfe791b22e78145
Uzyxen <michal912456@gmail.com>
2025-02-10 17:10:09 +0100

    add more product sections

--- pages/produkty/[product].vue
+++ pages/produkty/[product].vue
@@ -10,12 +10,12 @@
                     <img class="object-contain" :src="image.imageUrl" alt="">
                 </div>
             </div>
-            <div class="w-[600px] p-10">
+            <div class="w-[700px] p-8">
                 <img class="size-full object-contain" :src="product.images[currendImageIndex].imageUrl" alt="">
             </div>
 
-            <div class="flex gap-32 flex-1">
-                <div class="flex flex-col gap-2">
+            <div class="flex gap-32 flex-1 border-l border-blue-gray">
+                <div class="flex flex-col gap-2 pl-10">
                     <h1 class="text-2xl">{{ product.title }}</h1>
                     
                     <ProductRating :rating="4.5" />
@@ -42,6 +42,31 @@
                     </div>
                     <h4 class="text-sm">Ilość sztuk: 12</h4>
                 </div>
+            </div>
+        </div>
+
+        <div class="mt-14">
+            <ul class="flex border border-blue-gray">
+                <li class="hover:bg-blue-gray p-3 px-8">Opis</li>
+                <li class="hover:bg-blue-gray p-3 px-8">Specifykacja</li>
+                <li class="hover:bg-blue-gray p-3 px-8">Opinie</li>
+            </ul>
+        </div>
+
+        <div class="flex flex-col gap-10">
+            <div class="mt-10">
+                <h2 class="font-medium">Opis</h2>
+                <p class="break-words">{{ product.description }}</p>
+            </div>
+
+            <div>
+                <h2 class="font-medium">Specyfikacja</h2>
+                <p class="break-words">specyfikacja</p>
+            </div>
+
+            <div>
+                <h2 class="font-medium">Opinie</h2>
+                <p class="break-words">opinie</p>
             </div>
         </div>
     </section>
