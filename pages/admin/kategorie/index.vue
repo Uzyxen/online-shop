56e1c8f4bcba65b67f3a3381ab9f5b24682c2dc7
Uzyxen <michal912456@gmail.com>
2025-02-10 23:05:46 +0100

    display all categories

--- pages/admin/kategorie/index.vue
+++ pages/admin/kategorie/index.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
+    <section class="px-3 sm:px-5 md:px-10 lg:px-20">
+        <div class="bg-blue-gray" v-for="category in categories">
+            <h1 class="p-2">{{ category.title }}</h1>
 
-    </div>
+            <div class="bg-blue-gray-light pl-5" v-for="subcategory in category.subcategories">
+                <h2 class="p-2">{{ subcategory.title }}</h2>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup>
-
+    const { categories } = useStore();
 </script>
