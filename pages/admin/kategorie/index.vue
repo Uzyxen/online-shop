c68c678dcab83bc5ea9d0feb71aeb15e3bba6fcb
Uzyxen <michal912456@gmail.com>
2025-02-10 23:09:32 +0100

    hide subcategories by default

--- pages/admin/kategorie/index.vue
+++ pages/admin/kategorie/index.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="px-3 sm:px-5 md:px-10 lg:px-20">
-        <div class="bg-blue-gray" v-for="category in categories">
+        <div class="bg-blue-gray" v-for="(category, index) in categories" @click="isCollapsed[index] = !isCollapsed[index]">
             <h1 class="p-2">{{ category.title }}</h1>
 
-            <div class="bg-blue-gray-light pl-5" v-for="subcategory in category.subcategories">
+            <div v-show="isCollapsed[index] === true" class="bg-blue-gray-light pl-5" v-for="subcategory in category.subcategories">
                 <h2 class="p-2">{{ subcategory.title }}</h2>
             </div>
         </div>
@@ -12,4 +12,11 @@
 
 <script setup>
     const { categories } = useStore();
+    const isCollapsed = ref([]);
+
+    onMounted(() => {
+        categories.value.forEach(() => {
+            isCollapsed.value.push(false);
+        });
+    });
 </script>
