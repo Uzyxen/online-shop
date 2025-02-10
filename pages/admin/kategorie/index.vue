7d6accead33f0a702f2bdb607aabc96339da7b7c
Uzyxen <michal912456@gmail.com>
2025-02-10 23:16:33 +0100

    styles

--- pages/admin/kategorie/index.vue
+++ pages/admin/kategorie/index.vue
@@ -1,10 +1,14 @@
 <template>
     <section class="px-3 sm:px-5 md:px-10 lg:px-20">
-        <div class="bg-blue-gray" v-for="(category, index) in categories" @click="isCollapsed[index] = !isCollapsed[index]">
-            <h1 class="p-2">{{ category.title }}</h1>
+        <div class="border border-blue-gray border-t-0 first:border-t hover:bg-blue-gray-light cursor-pointer" v-for="(category, index) in categories" @click="isCollapsed[index] = !isCollapsed[index]">
+            <div class="p-2 flex justify-between items-center">
+                <h1>{{ category.title }}</h1>
 
-            <div v-show="isCollapsed[index] === true" class="bg-blue-gray-light pl-5" v-for="subcategory in category.subcategories">
-                <h2 class="p-2">{{ subcategory.title }}</h2>
+                <NuxtIcon name="solar:alt-arrow-down-linear" mode="svg" size="2rem" :class="{ 'rotate-180': isCollapsed[index] === true }" />
+            </div>
+
+            <div v-show="isCollapsed[index] === true" class="bg-blue-gray-light" v-for="subcategory in category.subcategories">
+                <h2 class="p-3 pl-5 hover:underline cursor-pointer">{{ subcategory.title }}</h2>
             </div>
         </div>
     </section>
