d8b435e17d3b83b606f022a8600c90726ac33baf
Uzyxen <michal912456@gmail.com>
2025-02-10 23:36:58 +0100

    add more contrast

--- pages/admin/kategorie/index.vue
+++ pages/admin/kategorie/index.vue
@@ -1,14 +1,15 @@
 <template>
     <section class="px-3 sm:px-5 md:px-10 lg:px-20">
-        <div class="border border-blue-gray border-t-0 first:border-t hover:bg-blue-gray-light cursor-pointer" v-for="(category, index) in categories" @click="isCollapsed[index] = !isCollapsed[index]">
+        <div v-for="(category, index) in categories" @click="isCollapsed[index] = !isCollapsed[index]" :key="category.id" class="border border-blue-gray border-t-0 first:border-t hover:bg-blue-gray-light cursor-pointer">
             <div class="p-2 flex justify-between items-center">
                 <h1>{{ category.title }}</h1>
 
                 <NuxtIcon name="solar:alt-arrow-down-linear" mode="svg" size="2rem" :class="{ 'rotate-180': isCollapsed[index] === true }" />
             </div>
 
-            <div v-show="isCollapsed[index] === true" class="bg-blue-gray-light" v-for="subcategory in category.subcategories">
-                <h2 class="p-3 pl-5 hover:underline cursor-pointer">{{ subcategory.title }}</h2>
+            <div v-for="subcategory in category.subcategories" :key="subcategory.id" v-show="isCollapsed[index] === true" class="hover:bg-blue-gray-20 border-t border-blue-gray flex items-center gap-1 pl-5 relative before:content-[''] before:absolute before:-left-[1px] before:h-[calc(100%+2px)] before:w-2 before:bg-blue">
+                <NuxtIcon name="solar:forward-2-linear" mode="svg" size="1.2rem" />
+                <h2 class="p-3 hover:underline cursor-pointer">{{ subcategory.title }}</h2>
             </div>
         </div>
     </section>
