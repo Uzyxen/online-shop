45fe7781fcf5958b807a4377567d18889da1c0af
Uzyxen <michal912456@gmail.com>
2025-02-11 16:09:37 +0100

    display cart items info

--- components/blocks/CartProductBlock.vue
+++ components/blocks/CartProductBlock.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        {{ product }}
+    <div class="border border-blue-gray flex gap-2 mt-1">
+        <div class="h-16 w-16 p-2">
+            <img :src="product.image" class="size-full object-contain">
+        </div>
+
+        <div class="flex flex-col pb-1">
+            <h1 class="flex-1">{{ product.title }}</h1>
+            <h2 class="text-sm">{{ product.quantity }} szt</h2>
+        </div>
     </div>
 </template>
 
