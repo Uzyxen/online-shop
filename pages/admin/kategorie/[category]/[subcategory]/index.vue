ad027c9702f9323c08d80f9180cc860ba7d28809
Uzyxen <michal912456@gmail.com>
2025-02-14 22:29:52 +0100

    add button

--- pages/admin/kategorie/[category]/[subcategory]/index.vue
+++ pages/admin/kategorie/[category]/[subcategory]/index.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        {{ $route.params.category }}
-        {{ $route.params.subcategory }}
-    </div>
+    <section class="px-3 sm:px-5 md:px-10 lg:px-20">
+        <PrimaryButton>Dodaj właściwość</PrimaryButton>
+    </section>
 </template>
+
+<script setup>
+
+</script>
