37cfe792483324c1cf38d0a488762c4eadd47b34
Uzyxen <michal912456@gmail.com>
2025-02-16 22:06:16 +0100

    add input for searching for orders

--- pages/admin/zamowienia/index.vue
+++ pages/admin/zamowienia/index.vue
@@ -4,6 +4,11 @@
     </div>
 
     <section v-else class="px-3 sm:px-5 md:px-10 lg:px-20 mt-5">
+        <div class="flex gap-2">
+            <input type="text" class="outline-none border border-gray p-1" placeholder="ID zamówienia">
+            <PrimaryButton>Szukaj</PrimaryButton>
+        </div>
+
         <AppTable :headers="['Numer zamówienia', 'Status', 'Data', 'Cena', 'ID użytkownika']" class="mt-8">
             <OrderBlock v-for="order in orders.response" :order="order" :admin="true" :key="order.id" v-if="orders" />
         </AppTable>
