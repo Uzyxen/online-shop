306e3eee2bb3cfcf96839cba96265fc7714ec478
Uzyxen <michal912456@gmail.com>
2025-02-16 21:44:49 +0100

    fetch all orders

--- pages/admin/zamowienia/index.vue
+++ pages/admin/zamowienia/index.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>
-
+    <div v-if="status === 'pending'">
+        <Loader />
     </div>
+
+    <section v-else>
+        {{ orders }}
+    </section>
 </template>
 
 <script setup>
+    const { useAccessToken } = useStore();
+    const token = useAccessToken();
 
+    const { status, data: orders } = await useLazyFetch('/api/orders', {
+        headers: {
+            authorization: `Bearer ${token.value}`
+        }
+    });
 </script>
