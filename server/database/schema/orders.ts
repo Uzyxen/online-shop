2f42528269d4db5a59af9c081e2783fa52f736ae
Uzyxen <michal912456@gmail.com>
2025-02-16 21:47:45 +0100

    fix schema types

--- server/database/schema/orders.ts
+++ server/database/schema/orders.ts
@@ -1,14 +1,14 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { date, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 import { users } from "./users";
 
 export const orders = pgTable('orders', {
     id: integer().notNull().primaryKey().generatedByDefaultAsIdentity(),
     userId: integer().references(() => users.id, { onDelete: 'cascade' }),
-    date: integer().notNull(),
-    status: integer().notNull(),
+    date: date().notNull(),
+    status: varchar({ length: 50 }).notNull(),
     total: integer().notNull(),
-    address: integer().notNull(),
-    payment: integer().notNull(),
-    delivery: integer().notNull(),
-    comment: integer().notNull(),
+    address: varchar().notNull(),
+    payment: varchar().notNull(),
+    delivery: varchar().notNull(),
+    comment: varchar().notNull(),
 });
