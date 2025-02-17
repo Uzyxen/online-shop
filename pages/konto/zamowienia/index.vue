5bf29ac6dd6ecb939ab979b8d215a05d6eaeac1b
Uzyxen <michal912456@gmail.com>
2025-02-17 20:57:44 +0100

    display info if no orders

--- pages/konto/zamowienia/index.vue
+++ pages/konto/zamowienia/index.vue
@@ -6,9 +6,13 @@
     <section class="px-3 sm:px-5 md:px-10 lg:px-20 mt-5" v-else>
         <h1 class="text-3xl">Zamówienia</h1>
 
-        <AppTable :headers="['Numer zamówienia', 'Status', 'Data', 'Cena']" class="mt-8">
+        <AppTable :headers="['Numer zamówienia', 'Status', 'Data', 'Cena']" class="mt-8" v-if="orders.response.length">
             <OrderBlock v-for="order in orders.response" :order="order" :admin="false" :key="order.id" />
         </AppTable>
+
+        <div v-else>
+            <p class="mt-5">Brak zamówień</p>
+        </div>
     </section>
 </template>
 
