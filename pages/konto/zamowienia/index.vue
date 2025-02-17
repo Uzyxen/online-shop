1afc6efd4861c2c0ae54944703aa0662ad3fc2b4
Uzyxen <michal912456@gmail.com>
2025-02-17 20:50:10 +0100

    fetch user orders

--- pages/konto/zamowienia/index.vue
+++ pages/konto/zamowienia/index.vue
@@ -1,17 +1,24 @@
 <template>
-    <section class="px-3 sm:px-5 md:px-10 lg:px-20 mt-5">
+    <div v-if="status === 'pending'">
+        <Loader />
+    </div>
+
+    <section class="px-3 sm:px-5 md:px-10 lg:px-20 mt-5" v-else>
         <h1 class="text-3xl">Zamówienia</h1>
 
         <AppTable :headers="['Numer zamówienia', 'Status', 'Data', 'Cena']" class="mt-8">
-            <OrderBlock v-for="order in orders" :order="order" :admin="false" :key="order.id" />
+            <OrderBlock v-for="order in orders.response" :order="order" :admin="false" :key="order.id" />
         </AppTable>
     </section>
 </template>
 
-<script lang="ts" setup>
-    const orders = ref<Order[]>([
-        { id: 1, status: 'placed', date: '2021-10-10', price: 4760 },
-        { id: 2, status: 'cancelled', date: '2021-10-11', price: 210 },
-        { id: 3, status: 'progress', date: '2021-10-12', price: 390 },
-    ]);
+<script setup>
+    const { useAccessToken } = useStore();
+    const token = useAccessToken();
+
+    const { status, data: orders } = await useLazyFetch('/api/orders/user', {
+        headers: {
+            authorization: `Bearer ${token.value}`
+        }
+    });
 </script>
